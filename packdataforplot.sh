25cbf7dbb9e2cea92d90931d91213814ac4d1461
biaowang <biaowang@mailbox.aes.tu-berlin.de>
2014-06-17 22:43:08 +0200

    add new function in packdataforplot.sh

--- packdataforplot.sh
+++ packdataforplot.sh
@@ -17,3 +17,24 @@ packdataforplot(){
     rm ydata.dat
     rm total.dat
 }
+
+
+packdataforplotwithX(){
+    Yheader=$1
+    Xheader=$2
+    if=$3
+    of=$4
+    if [[ -f $of ]]
+        then
+        mv $of $of.backup
+    fi
+    echo $Yheader >> $of
+    sed 's/|//g' $if > ydata.dat
+    LINENUMBER=`cat ydata.dat | wc -l`
+    echo $Xheader |tr ' ' '\n' > xdata.dat
+    paste -d ' ' xdata.dat ydata.dat > total.dat
+    cat total.dat >>$of
+    rm xdata.dat
+    rm ydata.dat
+    rm total.dat
+}
